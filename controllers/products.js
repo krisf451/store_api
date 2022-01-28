24202930f84742f639c8643734837c5677b467a0
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-27 17:38:25 -0800

    mvp for now

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,13 @@
 const Product = require("../models/product");
 
+//TODO: Set up CRUD routes
+
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({}).sort("name");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, name, company, sort, fields, limit } = req.query;
+  const { featured, name, company, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -17,7 +19,7 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  // console.log(queryObject);
+
   let result = Product.find(queryObject);
 
   //sort
@@ -32,10 +34,34 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
-  //pagination
-  if (limit) {
-    result = result.limit(Number(limit));
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
