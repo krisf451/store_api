de8938dcbf54eeff33ebbc044819beef3ba3b802
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-27 16:23:28 -0800

    added sort, limit, and fields(select) queries to getAllProducts endpoint

--- controllers/products.js
+++ controllers/products.js
@@ -1,26 +1,42 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "vase table",
-  });
+  const products = await Product.find({}).sort("name");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, name, company } = req.query;
+  const { featured, name, company, sort, fields, limit } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
   if (name) {
-    queryObject.name = name;
+    queryObject.name = { $regex: name, $options: "i" };
   }
   if (company) {
     queryObject.company = company;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  //pagination
+  if (limit) {
+    result = result.limit(Number(limit));
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
